1f1d5b30be157ddd050a246cb5e4981a433490ee
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2021-12-02 00:49:14 +0100

    change webpack mode to prod and rebuild

--- src/utils/disableButtonHelpers.ts
+++
@@ -1,7 +0,0 @@
-export function enableButton(buttonElement: HTMLButtonElement) {
-  buttonElement.disabled = false;
-}
-
-export function disableButton(buttonElement: HTMLButtonElement) {
-  buttonElement.disabled = true;
-}
